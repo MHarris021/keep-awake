54bfe1f49703ba0b7bba5d3a1e415a1bef3e47ad
Michael Harris <mharris@darcstar-solutions.tech>
2023-11-08 14:44:40 -0600

    initial commit

---
+++ background.js
@@ -0,0 +1,70 @@
+
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.storage.local.set({ keepAwakeEnabled: false });
+});
+
+chrome.alarms.onAlarm.addListener((alarm) => {
+    if (alarm.name === 'checkSchedule') {
+        checkSchedule();
+    }
+});
+
+function checkSchedule() {
+    chrome.storage.local.get(['keepAwakeEnabled', 'startTime', 'endTime'], function(result) {
+        if (result.keepAwakeEnabled) {
+            chrome.power.requestKeepAwake('display');
+            return;
+        }
+
+        const now = new Date();
+        const start = new Date();
+        const end = new Date();
+        const [startHours, startMinutes] = result.startTime.split(':').map(Number);
+        const [endHours, endMinutes] = result.endTime.split(':').map(Number);
+        start.setHours(startHours, startMinutes);
+        end.setHours(endHours, endMinutes);
+
+        // Check if the current time is within the schedule
+        if (now >= start && now <= end) {
+            chrome.power.requestKeepAwake('display');
+        } else {
+            chrome.power.releaseKeepAwake();
+        }
+    });
+}
+
+function createAlarm() {
+    chrome.alarms.create('checkSchedule', {periodInMinutes: 1});
+}
+
+function clearAlarm() {
+    chrome.storage.local.get(['keepAwakeEnabled', 'startTime', 'endTime'], function(result) {
+        if(!result.keepAwakeEnabled&& (!result.startTime && !result.endTime)) {
+            chrome.alarms.clear('checkSchedule');
+        }
+    });
+}
+
+function toggleKeepAwake(state) {
+    chrome.storage.local.set({ keepAwakeEnabled: state }, function() {
+        if(state){
+            createAlarm();
+        }
+    });
+}
+
+// Listen for a message from the options page
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        if (request.message === "toggle") {
+            toggleKeepAwake(request.state);
+            sendResponse({status: "success"});
+        } else if (request.message === "setSchedule") {
+            createAlarm();
+            sendResponse({status: "scheduleSet"});
+        } else if (request.message === "clearSchedule") {
+            clearAlarm();
+            sendResponse({status: "scheduleCleared"});
+        }
+    }
+);
---
+++ options/options.js
@@ -0,0 +1,57 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    var checkbox = document.getElementById('toggleAwake');
+    var setScheduleButton = document.getElementById('setSchedule');
+    var clearScheduleButton = document.getElementById('clearSchedule');
+    var startTimeInput = document.getElementById('startTime');
+    var endTimeInput = document.getElementById('endTime');
+
+    // Load the initial state of the checkbox and the schedule
+    chrome.storage.local.get(['keepAwakeEnabled', 'startTime', 'endTime'], function(result) {
+        checkbox.checked = result.keepAwakeEnabled || false;
+        startTimeInput.value = result.startTime || '';
+        endTimeInput.value = result.endTime || '';
+    });
+
+    // Add a listener for when the checkbox changes
+    checkbox.addEventListener('change', function() {
+        chrome.runtime.sendMessage({message: "toggle", state: checkbox.checked}, function(response) {
+            console.log(response.status);
+        });
+    });
+
+    // Add a listener for when the Set Schedule button is clicked
+    setScheduleButton.addEventListener('click', function() {
+        const startTime = startTimeInput.value;
+        const endTime = endTimeInput.value;
+        // Save the schedule times
+        chrome.storage.local.set({startTime: startTime, endTime: endTime}, function() {
+            console.log('Schedule set:', startTime, endTime);
+            // Send a message to the background page to update the alarm
+            chrome.runtime.sendMessage({message: "setSchedule"}, function(response) {
+                if(response.status === "scheduleSet") {
+                    // Show a success message
+                    alert('Schedule has been set: ' + startTime + ' to ' + endTime);
+                }
+                console.log(response.status);
+            });
+        });
+    });
+
+    // Add a listener for when the Clear Schedule button is clicked
+    clearScheduleButton.addEventListener('click', function() {
+        // Clear the schedule times
+        chrome.storage.local.remove(['startTime', 'endTime'], function() {
+            startTimeInput.value = '';
+            endTimeInput.value = '';
+            // Send a message to the background page to clear the alarm
+            chrome.runtime.sendMessage({message: "clearSchedule"}, function(response) {
+                if (response.status === "scheduleCleared") {
+                    // Show a success message
+                    alert('Schedule has been cleared.');
+                }
+                console.log(response.status);
+            });
+        });
+    });
+});
